8bc089f79fea0b9ab9b1b03e1f14878b41de43cd
Mark Gibson <mgibson@adaptavist.com>
2015-10-17 21:15:48 +0100

    update d.ts to reflect recent API changes

--- src/fluxlet.d.ts
+++ src/fluxlet.d.ts
@@ -9,75 +9,64 @@
 //     if you don't want to create an interface to declare all dispatchers,
 //     then just use Dispatchers.
 
-declare function fluxlet<S,D>(id?: string, opts?: FluxletOptions): Fluxlet<S,D>
+declare function fluxlet<S,D>(id?: string): Fluxlet<S,D>
 
 interface Fluxlet<S,D> {
-    validator(validator: Validator<S>): Fluxlet<S,D>
+    hooks(...namedHooks: Array<NamedHooks<S,D>>): Fluxlet<S, D>
 
-    state(state: S): Fluxlet<S,D>
+    state(state: StateInitializer<S>): Fluxlet<S,D>
 
-    actions(namedActions: Named<Action<S>|ConditionalAction<S>>): Fluxlet<S,D>
+    actions(...namedActions: Array<Named<Action<S>>>): Fluxlet<S,D>
 
-    calculations(namedCalculations: Named<Calculation<S>|ConditionalCalculation<S>>): Fluxlet<S,D>
+    calculations(...namedCalculations: Array<Named<Calculation<S>>>): Fluxlet<S,D>
 
-    sideEffects(namedSideEffects: Named<SideEffect<S,D>|ConditionalSideEffect<S,D>>): Fluxlet<S,D>
+    sideEffects(...namedSideEffects: Array<Named<SideEffect<S,D>>>): Fluxlet<S,D>
 
-    init(fn: Init<D>): Fluxlet<S,D>
-
-    logging(categories: Logging): Fluxlet<S,D>
+    init(...fn: Array<Init<D>>): Fluxlet<S,D>
 
     remove(): void
 }
 
-interface FluxletOptions {
-    params: string[]
-}
+declare type StateInitializer<S> = (state: S | void) => S
 
-interface Validator<S> {
-    (state: S): void
-}
-
-interface Action<S> {
+interface ActionFn<S> {
     (...args: any[]): (state: S) => S
 }
 
-interface ConditionalAction<S> {
+interface ActionObj<S> {
     when?: (state: S, ...args: any[]) => boolean
-    then: Action<S>
+    then: ActionFn<S>
 }
 
-interface Calculation<S> {
+declare type Action<S> = ActionFn<S> | ActionObj<S>
+
+interface CalculationFn<S> {
     (state: S, prevState: S): S
 }
 
-interface ConditionalCalculation<S> {
+interface CalculationObj<S> {
     requiresCalculations?: string|string[]
     when?: (state: S, prevState: S) => boolean
-    then: Calculation<S>
+    then: CalculationFn<S>
 }
 
-interface SideEffect<S,D> {
-    (state: S, prevState: S, dispatchers: D): void
+declare type Calculation<S> = CalculationFn<S> | CalculationObj<S>
+
+interface SideEffectFn<S,D> {
+    (state: S, prevState: S, dispatchers: D): void | any
 }
 
-interface ConditionalSideEffect<S,D> {
+interface SideEffectObj<S,D> {
     requiresCalculations?: string|string[]
     requiresSideEffects?: string|string[]
     when?: (state: S, prevState: S) => boolean
-    then: SideEffect<S,D>
+    then: SideEffectFn<S,D>
 }
+
+declare type SideEffect<S,D> = SideEffectFn<S,D> | SideEffectObj<S,D>
 
 interface Init<D> {
     (dispatchers: D): void
-}
-
-interface Logging {
-    register?: boolean
-    dispatch?: boolean
-    call?: boolean
-    args?: boolean
-    state?: boolean
-    timing?: boolean
 }
 
 interface Dispatcher {
@@ -89,3 +78,78 @@ interface Named<T> {
 }
 
 declare type Dispatchers = Named<Dispatcher>
+
+interface NamedHooks<S,D> {
+  registerState?: Hook<RegisterStateHookParams<S>, S>
+
+  registerAction?: Hook<RegisterHookParams, Action<S>>
+  registerDispatcher?: Hook<RegisterHookParams, Dispatcher>
+  registerCalculation?: Hook<RegisterHookParams, Calculation<S>>
+  registerSideEffect?: Hook<RegisterHookParams, SideEffect<S,D>>
+
+  dispatch?: Hook<DispatchHookParams<S>, S>
+  action?: Hook<ActionHookParams<S>, S>
+  calculations?: Hook<CalculationsHookParams<S>, S>
+  calculation?: Hook<CalculationHookParams<S>, S>
+  sideEffects?: Hook<SideEffectsHookParams<S>, void>
+  sideEffect?: Hook<SideEffectHookParams<S,D>, any>
+}
+
+interface Hook<P, V> {
+  (params: P): void | PostHook<V>
+}
+
+interface PostHook<V> {
+  (value: V): void | V
+}
+
+interface CommonHookParams {
+  logId: string
+}
+
+interface RegisterStateHookParams<S> extends CommonHookParams {
+  state: StateInitializer<S>
+  lockedState: S
+}
+
+interface RegisterHookParams extends CommonHookParams {
+  name: string
+}
+
+interface ActionHookParams<S> extends CommonHookParams {
+  actionName: string
+  actionArgs: any[]
+  startState: S
+}
+
+interface DispatchHookParams<S> extends ActionHookParams<S> {
+  enable: boolean
+}
+
+interface CalculationsHookParams<S> extends CommonHookParams {
+  actionName: string
+  startState: S
+  transientState: S
+}
+
+interface CalculationHookParams<S> extends CommonHookParams {
+  actionName: string
+  calculation: Calculation<S>
+  startState: S
+  priorState: S
+  enable: boolean
+}
+
+interface SideEffectsHookParams<S> extends CommonHookParams {
+  actionName: string
+  lockedState: S
+}
+
+interface SideEffectHookParams<S,D> extends CommonHookParams {
+  actionName: string
+  sideEffect: SideEffect<S,D>
+  startState: S
+  lockedState: S
+  dispatchers: D
+  enable: boolean
+}
